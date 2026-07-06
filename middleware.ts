252ffd16e196b77b222fdf9a0d5cd8f4c2af04db
developmentalhub <robyn@playmoveimprove.com.au>
2026-07-06 10:05:12 +1000

    Redirect draft topic pages until videos are ready

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const isDraftTopicPage =
+    pathname.startsWith("/topics/") && pathname !== "/topics";
+
+  if (isDraftTopicPage) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/topics";
+    redirectUrl.search = "";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/topics/:path*"],
+};
